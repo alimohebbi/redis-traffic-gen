09ccaac6f474269d02a4ee7ac4a027345551f84d
ali mohebbi <ali.mohebbi@usi.ch>
2019-11-19 13:52:23 +0100

    gradual thread increse

--- generator.py
+++ generator.py
@@ -1,6 +1,7 @@
 import datetime
 import logging
 from itertools import repeat
+from time import sleep
 
 import concurrent.futures as cf
 from tqdm import tqdm
@@ -23,4 +24,7 @@ def generate():
     start_time = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
     for load in tqdm(config.profile, desc='Test Progress'):
         with cf.ThreadPoolExecutor(max_workers=load) as executor:
-            executor.map(request_thread, range(load), repeat(start_time))
+            # executor.map(request_thread, range(load), repeat(start_time))
+            for i in tqdm(range(load), desc='Step progress'):
+                executor.submit(request_thread, i, start_time)
+                sleep(1)
